88f1a0d9db991930f80cb6ada4e66846d511b69a
Olowskyyy <oliwer.biznes@op.pl>
2025-05-05 23:17:39 +0200

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import './globals.css';
-export const metadata = { title: 'eBook Generator', description: 'Create beautiful eBooks easily' };
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
