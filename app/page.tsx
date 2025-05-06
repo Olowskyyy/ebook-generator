02aa46e672fbb1c1821faf7b124d72910eb2d9a2
Olowskyyy <oliwer.biznes@op.pl>
2025-05-06 23:07:20 +0200

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Welcome to the E-book Generator!</h1>
-      <p>This is a test deployment to confirm everything works.</p>
-    </div>
-  );
-}
