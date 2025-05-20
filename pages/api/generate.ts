326bdeb5c7e89b96a293341b24be269ff17f949a
Olowskyyy <oliwer.biznes@op.pl>
2025-05-20 17:52:54 +0200

    Delete pages directory

--- pages/api/generate.ts
+++
@@ -1,9 +0,0 @@
-
-import { NextApiRequest, NextApiResponse } from 'next';
-import { generateEbook } from '../../backend/generateEbook';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { content } = req.body;
-  const result = await generateEbook(content);
-  res.status(200).json({ result });
-}
