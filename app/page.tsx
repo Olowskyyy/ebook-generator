4fd4e40aa26fdbf39f5e7a4b0c723c477ae2d72c
Olowskyyy <oliwer.biznes@op.pl>
2025-05-20 17:51:13 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,10 @@
+import EbookForm from "../frontend/components/EbookForm";
 
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to the eBook Generator</h1>
+      <EbookForm />
+    </main>
+  );
+}
